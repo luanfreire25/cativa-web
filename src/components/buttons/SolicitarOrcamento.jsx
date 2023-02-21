81e1b501b0abc186c3b77d1a1acfaec464ef159f
Luan Freire <lunfreire@projectbox.pt>
2023-02-21 17:41:35 -0300

    estilizando o botão de solicitar orçamento.

--- src/components/buttons/SolicitarOrcamento.jsx
+++ src/components/buttons/SolicitarOrcamento.jsx
@@ -1,10 +1,25 @@
 import Button from "@mui/material/Button";
 import Heart from "../icons/Heart";
+import "../../styles/Button.css";
 
 const SolicitarOrcamento = () => {
   return (
     <>
-      <Button variant="outlined" startIcon={<Heart />}>
+      <Button
+        style={{
+          background: "#FF4F55",
+          color: "#fff",
+          boxShadow: "0px 0px 20px #FF4F55",
+          margin: "20px",
+          padding: "5px 20px",
+          border: "none",
+          borderRadius: "15px",
+          textTransform: "none",
+          fontSize: "17px"
+        }}
+        variant="outlined"
+        startIcon={<Heart />}
+      >
         Solicitar Orçamento
       </Button>
     </>
